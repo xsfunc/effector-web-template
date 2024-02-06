81ac7ae85df81d8fc5f7d661db06b1891c993151
xsfunc <xsfunc@gmail.com>
2024-02-06 17:18:15 +0300

    build: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import swc from 'unplugin-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    swc.vite({
-      jsc: {
-        experimental: {
-          plugins: [
-            [
-              '@effector/swc-plugin',
-              { factories: ['@withease/factories'] },
-            ],
-          ],
-        },
-      },
-    }),
-  ],
+  plugins: [],
 
   resolve: {
     alias: {
